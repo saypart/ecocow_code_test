2c291482441561095d9ee1620bad0b8b45e9f464
sanghyeon <saypart@gmail.com>
2024-08-03 15:08:26 +0900

    초기 세팅 업데이트

---
+++ src/main/java/codeTest/ecocow/EcocowApplication.java
@@ -0,0 +1,13 @@
+package codeTest.ecocow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcocowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcocowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeTest/ecocow/EcocowApplicationTests.java
@@ -0,0 +1,13 @@
+package codeTest.ecocow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcocowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
